0515909a86554e73d56361e3ad7a612799917732
Steven <1477010+stnguyen90@users.noreply.github.com>
2022-07-14 23:54:03 +0000

    Update server to return Hello, World!

--- src/index.php
+++ src/index.php
@@ -7,33 +7,26 @@ if (file_exists(__DIR__.'/../vendor/autoload.php')) {
 use Utopia\App;
 use Utopia\Swoole\Request;
 use Utopia\Swoole\Response;
-use Utopia\Swoole\Files;
 use Swoole\Http\Server;
 use Swoole\Http\Request as SwooleRequest;
 use Swoole\Http\Response as SwooleResponse;
 
-$http = new Server("0.0.0.0", 80);
+App::get('/') // Define Route
+    ->inject('request')
+    ->inject('response')
+    ->action(
+        function(Request $request, Response $response) {
+            $response->send("Hello, World!\n");
+        }
+    );
 
-Files::load(__DIR__ . '/../public'); // Static files location
+$http = new Server("0.0.0.0", 8080);
 
 $http->on('request', function (SwooleRequest $swooleRequest, SwooleResponse $swooleResponse) {
     $request = new Request($swooleRequest);
     $response = new Response($swooleResponse);
 
-    if(Files::isFileLoaded($request->getURI())) { // output static files with cache headers
-        $time = (60 * 60 * 24 * 365 * 2); // 45 days cache
-
-        $response
-            ->setContentType(Files::getFileMimeType($request->getURI()))
-            ->addHeader('Cache-Control', 'public, max-age='.$time)
-            ->addHeader('Expires', \date('D, d M Y H:i:s', \time() + $time).' GMT') // 45 days cache
-            ->send(Files::getFileContents($request->getURI()))
-        ;
-
-        return;
-    }
-
-    $app = new App('Asia/Tel_Aviv');
+    $app = new App('UTC');
     
     try {
         $app->run($request, $response);
