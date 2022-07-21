de3f82a81675a2f268a1a08d444f88e6d71d1526
Steven <stnguyen90@users.noreply.github>
2022-07-21 22:43:48 +0000

    Add GET /todos path

--- src/index.php
+++ src/index.php
@@ -20,6 +20,33 @@ App::get('/') // Define Route
         }
     );
 
+App::setResource('todos', function() {
+    $path = \realpath('/app/src/todos.json');
+    $data = \json_decode(\file_get_contents($path));
+    return $data;
+});
+
+App::shutdown(function($todos) {
+    \print_r($todos);
+    $path = \realpath('todos.json');
+    $jsonData = json_encode($todos, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+    file_put_contents($path, $jsonData);
+}, ['todos']);
+
+App::get('/todos')
+    ->inject('request')
+    ->inject('response')
+    ->inject('todos')
+    ->action(
+        function(Request $request, Response $response, $todos) {
+            $data = [];
+            foreach ($todos as $id => $todo) {
+                $data[] = $todo;
+            }
+            $response->json($data);
+        }
+    );
+
 $http = new Server("0.0.0.0", 8080);
 
 $http->on('request', function (SwooleRequest $swooleRequest, SwooleResponse $swooleResponse) {
